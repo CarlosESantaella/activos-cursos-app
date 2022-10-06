6e9025c9d2e11adc8d951eb9ece09810885580e5
Hildegar Medina <medinahildegar1@gmail.com>
2022-10-06 16:48:21 -0300

    Auth login

--- controllers/login_view.php
+++ controllers/login_view.php
@@ -1,7 +1,15 @@
 <?php 
     require_once($_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php');
+    require_once($_SERVER['DOCUMENT_ROOT'].'/controllers/actions/auth.php');
+
+    use Controller\Auth;
 
     $dotenv = Dotenv\Dotenv::createImmutable($_SERVER['DOCUMENT_ROOT']);
     $dotenv->load();
+
+    if (isset($_COOKIE["access_token"])) {
+        $user_global = Auth::me($_COOKIE["access_token"]);
+        if ($user_global) header("location:/");
+    }
 
     include($_SERVER['DOCUMENT_ROOT'].'/views/login_view.php');
